6e9c0e319aece15b7667faf07f43eadf126e408d
Maksat Alymkulov <maksat.alymkulov@gmail.com>
2024-09-27 01:27:09 -0700

    change from local data to URL from MockApi

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 //ESM
 import express from "express";
-import { users } from "./data.js";
+import axios from "axios";
+// import { users } from "./data.js";
 
-console.log(users);
+// console.log(users);
 //CommonJS
 // const express = require("express");
 const app = express();
+const url = "https://66f668e3436827ced97701db.mockapi.io/students";
 const port = process.env.PORT || 8000;
 
 app.get("/", async (req, res) => {
-  res.send({
+  //   res.send({
+  //     message: "Welcome to Homepage",
+  //     total_Users: users.length,
+  //     users: users,
+  //   });
+  res.status(200).json({
     message: "Welcome to Homepage",
-    total_Users: users.length,
-    users: users,
   });
+});
+
+app.get("/students", async (req, res) => {
+  try {
+    const response = await axios.get(url);
+    res.status(200).json({
+      message: "List of Students",
+      total_Students: response.data.length,
+      students: response.data,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Data not found",
+    });
+  }
 });
 
 app.listen(port, () => {
