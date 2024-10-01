53af68bdeb40ae5ba5916ec9ba2bf7821e2d0435
Maksat Alymkulov <maksat.alymkulov@gmail.com>
2024-09-30 22:54:57 -0700

    adding POST and DELETE methods

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-//ESM
 import express from "express";
 import axios from "axios";
-// import { users } from "./data.js";
+import "dotenv/config";
+import { v4 as uuidv4 } from "uuid";
 
+//* import{readFile, writeFile} from "node:fs/promises"
+
+//ESM
+// import { users } from "./data.js";
+// require('dotenv').config()
 // console.log(users);
 //CommonJS
 // const express = require("express");
 const app = express();
-const url = "https://66f668e3436827ced97701db.mockapi.io/students";
+const url = "https://66f668e3436827ced97701db.mockapi.io/students/";
 const port = process.env.PORT || 8000;
 
-app.get("/", async (req, res) => {
-  //   res.send({
-  //     message: "Welcome to Homepage",
-  //     total_Users: users.length,
-  //     users: users,
-  //   });
+app.use(express.json());
+
+//! GET
+
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "Welcome to Homepage",
   });
@@ -35,6 +39,69 @@ app.get("/students", async (req, res) => {
     });
   }
 });
+
+app.get("/students/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await fetch(url + id);
+
+    const data = await response.json();
+    res.status(200).json({
+      message: "Student Found",
+      student: data,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Data not found",
+    });
+  }
+});
+
+//! DELETE
+
+app.delete("/students/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await axios.delete(url + id);
+
+    const deletedStudent = response.data.find((student) => student.id == id);
+    const newData = response.data.filter((student) => student.id != id);
+
+    response.data = newData;
+
+    res.status(200).json({
+      message: "Student has been deleted",
+      deletedStudent: deletedStudent,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Data not found",
+    });
+  }
+});
+
+//! POST
+
+app.post("/students", async (req, res) => {
+  const newStudent = req.body;
+  newStudent.id = uuidv4();
+  try {
+    const response = await axios.post(url, newStudent);
+
+    response.data.push(newStudent);
+
+    res.status(200).json({
+      message: "Student has been successfully added",
+      student: response.data,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error adding student",
+    });
+  }
+});
+
+//! LISTEN
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
